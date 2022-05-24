00ca1a69a34b97bcb7247a7e6133e7573e4f4786
Rusewww <Rusewww1@gmail.com>
2022-05-24 23:30:58 +0300

    t01

--- sprint07/t01_hack_it/index.js
+++ sprint07/t01_hack_it/index.js
@@ -1,39 +1,75 @@
 const express = require('express')
-let bodyParser = require('body-parser')
+const expressThymeleaf = require('express-thymeleaf')
+const {TemplateEngine} = require('thymeleaf')
+const bodyParser = require('body-parser')
 const session = require('express-session')
+const bcrypt = require('bcrypt')
+const PORT = process.env.PORT ?? 3000
 
-let sess
-let app = express()
+const app = express()
+const templateEngine = new TemplateEngine()
+
+app.engine('html', expressThymeleaf(templateEngine))
+app.set('view engine', 'html')
+app.set('views', __dirname + '/')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(
     session({
-        secret: 'session secret',
+        secret: 'password secret',
         saveUninitialized: true
     })
 )
 
-let urlencodedParser = bodyParser.urlencoded({extended: false})
+let sess
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}...`)
+})
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/save', urlencodedParser, function (req, res) {
-    let response = {
-        name: req.body.name,
-        email: req.body.email,
-        age: req.body.age,
-        about: req.body.about,
-        photo: req.body.photo
-    };
     sess = req.session
-
-    sess.copy = response
-
-    console.log(sess.copy);
-    res.end(JSON.stringify(sess.copy));
+    if (sess.hash && sess.hash !== '') {
+        return res.render('hash', {
+            hash: sess.hash
+        })
+    }
+    res.render('index')
 })
 
-app.listen(3000, () => {
-    console.log(`Server has been started`);
+app.post('/', (req, res) => {
+    if (!req.body) {
+        return res.sendStatus(400)
+    }
+    let salt = bcrypt.genSaltSync(Number(req.body.salt))
+    let password = bcrypt.hashSync(req.body.savePassword, salt)
+
+    req.session.hash = password
+    res.render('hash', {
+        hash: password
+    })
 })
+
+app.post('/check', (req, res) => {
+    if(!req.body) {
+        return res.sendStatus(400)
+    }
+    sess = req.session
+    if (bcrypt.compareSync(req.body.newPassword, sess.hash)) {
+        sess.hash = ''
+        return res.render('index', {
+            answer: 'Hacked!'
+        })
+    }
+    res.render('hash', {
+        answer: 'Access denied!',
+        hash: sess.hash
+    })
+})
+
+app.post('/clear', (req, res) => {
+    req.session.hash = ''
+    res.redirect('/')
+})
+
 
