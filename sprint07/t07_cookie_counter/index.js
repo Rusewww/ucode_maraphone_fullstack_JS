9ddc2ea3aaa8a7d4010d6bb03422ba2438763932
Rusewww <Rusewww1@gmail.com>
2022-05-24 23:38:15 +0300

    t07

--- sprint07/t07_cookie_counter/index.js
+++ sprint07/t07_cookie_counter/index.js
@@ -1,39 +1,35 @@
-const express = require('express')
-let bodyParser = require('body-parser')
-const session = require('express-session')
+onst express = require('express')
+const cookieSession = require('cookie-session')
+const expressThymeleaf = require('express-thymeleaf')
+const {TemplateEngine} = require('thymeleaf')
+const PORT = process.env.PORT ?? 8080
 
-let sess
-let app = express()
-app.use(
-    session({
-        secret: 'session secret',
-        saveUninitialized: true
-    })
-)
+const app = express()
+const templateEngine = new TemplateEngine()
 
-let urlencodedParser = bodyParser.urlencoded({extended: false})
+app.engine('html', expressThymeleaf(templateEngine))
+app.set('view engine', 'html')
+app.set('views', __dirname + '/')
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}...`)
+})
+
+const moment = require('moment')
+let date = new Date()
+let updateCounter = moment(date).add(1, 'm').toDate()
+
+app.use(cookieSession({
+    name: 'counter',
+    keys: ['key1', 'key2'],
+    maxAge: 60000
+}))
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
+    if (Number(updateCounter) <= Number(new Date())) {
+        req.session.counter = 0;
+        updateCounter = moment(new Date()).add(1, 'm').toDate();
+    }
+    req.session.counter = (req.session.counter || 0) + 1
+    res.render('index', {counter: req.session.counter})
 })
-
-app.post('/save', urlencodedParser, function (req, res) {
-    let response = {
-        name: req.body.name,
-        email: req.body.email,
-        age: req.body.age,
-        about: req.body.about,
-        photo: req.body.photo
-    };
-    sess = req.session
-
-    sess.copy = response
-
-    console.log(sess.copy);
-    res.end(JSON.stringify(sess.copy));
-})
-
-app.listen(3000, () => {
-    console.log(`Server has been started`);
-})
-
