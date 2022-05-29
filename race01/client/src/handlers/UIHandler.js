d5e311bed38907acb052a21ce73fa959f06d91dc
Rusewww <Rusewww1@gmail.com>
2022-05-29 13:17:28 +0300

    UI

--- race01/client/src/handlers/UIHandler.js
+++ race01/client/src/handlers/UIHandler.js
@@ -1,9 +1,37 @@
+import ZoneHandler from "./ZoneHandler";
+
 export default class UIHandler{
     constructor(scene) {
+
+        this.zoneHandler = new ZoneHandler(scene);
+
+        this.buildZone = () => {
+            scene.dropZone = this.zoneHandler.renderZone(470,500);
+            this.zoneHandler.renderOutline(scene.dropZone);
+        }
+
+        this.buildPlayerAreas = () => {
+            //PlayerArea - change coordinates!
+            scene.playerHandArea = scene.add.rectangle(470, 860, 850, 230);
+            scene.playerHandArea.setStrokeStyle(4, 0x0000FF);
+            scene.playerDeckArea = scene.add.rectangle(1000, 860, 155, 215);
+            scene.playerDeckArea.setStrokeStyle(3, 0x66CCCC);
+
+            //EnemyArea - change coordinates!
+            scene.opponentHandArea = scene.add.rectangle(470, 135, 850, 230);
+            scene.opponentHandArea.setStrokeStyle(4, 0x0000FF);
+            scene.opponentDeckArea = scene.add.rectangle(1000, 135, 155, 215);
+            scene.opponentDeckArea.setStrokeStyle(3, 0x66CCCC);
+
+        }
+        //DealCardButton - change coordinates, makes better
         this.buildGameText = () => {
             scene.dealCards = scene.add.text(960, 445, "Deal Cards");
         }
+
         this.buildUI = () => {
+            this.buildPlayerAreas();
+            this.buildZone();
             this.buildGameText();
         }
 
--- race01/client/src/scenes/game.js
+++ race01/client/src/scenes/game.js
@@ -1,3 +1,8 @@
+import CardHandler from "../handlers/CardHandler";
+import DeckHandler from "../handlers/DeckHandler";
+import InteractiveHandler from "../handlers/InteractiveHandler";
+import SocketHandler from "../handlers/SocketHandler";
+import GameHandler from "../handlers/GameHandler"
 import UIHandler from "../handlers/UIHandler";
 
 export default class Game extends Phaser.Scene {
@@ -8,11 +13,22 @@ export default class Game extends Phaser.Scene {
     }
 
     preload(){
-
+        this.load.image('cyanCardBack', 'src/assets/CyanCardBack.png');
+        this.load.image('magentaCardBack', 'src/assets/MagentaCardBack.png');
+        this.load.image('cyanBoolean', 'src/assets/Cyan_Boolean3x.png');
+        this.load.image('magentaBoolean', 'src/assets/Magenta_Boolean3x.png');
+        this.load.image('cyanPing', 'src/assets/Cyan_Ping3x.png');
+        this.load.image('magentaPing', 'src/assets/Magenta_Ping3x.png');
     }
+
     create(){
+        this.CardHandler = new CardHandler();
+        this.DeckHandler = new DeckHandler(this);
+        this.GameHandler = new GameHandler(this);
+        this.SocketHandler = new SocketHandler(this);
         this.UIhandler = new UIHandler(this);
         this.UIhandler.buildUI();
+        this.InteractiveHandler = new InteractiveHandler(this);
     }
     uploaded(){
 
