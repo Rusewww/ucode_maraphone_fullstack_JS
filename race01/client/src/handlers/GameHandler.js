0b25419c905d6aae91df0051d8a32fae8fc52b61
Rusewww <Rusewww1@gmail.com>
2022-05-30 21:44:42 +0300

    beggining_of_gameplay

--- race01/client/src/handlers/GameHandler.js
+++ race01/client/src/handlers/GameHandler.js
@@ -1,4 +1,18 @@
 export default class GameHandler{
-    constructor() {
+    constructor(scene) {
+        this.gameState = "Initializing";
+        this.isMyTurn = false;
+        this.playerDeck = [];
+        this.opponentDeck = [];
+
+        this.changeTurn = () =>{
+            this.isMyTurn = !this.isMyTurn;
+            console.log(`isMyTurn: ${this.isMyTurn}`);
+
+        }
+        this.changeGameState = (gameState) => {
+            this.gameState = gameState;
+            console.log(`GameState: ${this.gameState}`);
+        }
     }
 }
